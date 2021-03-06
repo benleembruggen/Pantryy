c8b4026fdb63246186f89388a9b01a24159717d3
benleembruggen <benleembruggen@tpg.com.au>
2021-03-06 11:51:43 +1100

    Get pantry use state

--- client/src/Components/Pantry.js
+++ client/src/Components/Pantry.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Nav from './Nav';
+import PantryService from '../Services/PantryService';
+import { AuthContext } from '../Context/AuthContext';
 
-function Pantry() {
+const Pantry = (props) => {
+  const [pantry, setPantry] = useState([]);
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    PantryService.getPantry().then((data) => {
+      setPantry(data.pantry);
+      console.log(data.pantry);
+    });
+  }, []);
+
   return (
     <>
       <Nav />
       <h1>Pantry Page</h1>
     </>
   );
-}
+};
 
 export default Pantry;
