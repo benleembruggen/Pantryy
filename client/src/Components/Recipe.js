5ab97a3adc8f9a4badef5feb49fdf78f39c20260
JamesPuli <pulijames@outlook.com>
2021-03-06 12:26:45 +1100

    Adding Nav to recipe tab

--- client/src/Components/Recipe.js
+++ client/src/Components/Recipe.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Search from './Search';
 import RecipeList from './RecipeList';
+import Nav from './Nav';
 
 function Recipe() {
   return (
-    <Container >
-      <h1>Recipe Page</h1>
-      <div>
-        <Search isLarge={true} placeholder="Search Recipes"/>
-        <br />
-        <RecipeList />
-      </div>
-    </Container>
+    <>
+    <Nav />
+      <Container >
+        <h1>Recipe Page</h1>
+        <div>
+          <Search isLarge={true} placeholder="Search Recipes"/>
+          <br />
+          <RecipeList />
+        </div>
+      </Container>
+    </>
   );
 }
 
