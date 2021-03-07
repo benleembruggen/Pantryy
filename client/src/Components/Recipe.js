bc73f42aba8789a00dd7a6035916834977242822
JamesPuli <pulijames@outlook.com>
2021-03-07 14:05:21 +1100

    Loading Bars

--- client/src/Components/Recipe.js
+++ client/src/Components/Recipe.js
@@ -8,16 +8,22 @@ import RecipeService from '../Services/RecipeService';
 import FormGroup from '@material-ui/core/FormGroup';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     justifyContent: "space-between",
     flexDirection: "row",
+  },
+  loading: {
+    zIndex: theme.zIndex.drawer + 1,
   }
 }));
 
 function Recipe() {
   const [recipes, setRecipes] = useState(null);
+  const [isLoading, setLoading] = useState(false);
   const [searchText, setSearchText] = useState("spaghetti")
   const [checkboxStates, setCheckboxStates] = React.useState({
     vegetarian: false,
@@ -35,11 +41,13 @@ function Recipe() {
 
   // Auto search for pasta on page load
   useEffect(() => {
-    RecipeService.getRecipes('spaghetti').then(setRecipes);
+    sendSearchReq('spaghetti', checkboxStates)
+    //RecipeService.getRecipes('spaghetti').then(setRecipes);
   }, []);
 
   // Search function
   const sendSearchReq = (searchText, searchFilters) =>{
+    setLoading(true);
     RecipeService.getRecipes(searchText).then(recipes => {
       const filteredRecipes = recipes.filter((recipe) => {
         if(searchFilters.vegetarian && !recipe.recipe.healthLabels.includes('Vegetarian')) return false;
@@ -48,7 +56,7 @@ function Recipe() {
         return true;
       });
       setRecipes(filteredRecipes);
-      console.log("done",searchFilters)
+      setLoading(false);
      });
   }
 
@@ -75,7 +83,7 @@ function Recipe() {
       }} placeholder='Search Recipes e.g. pasta'/>
       </FormGroup>
         <br />
-        <RecipeList recipes={recipes} />
+        {isLoading ? <><CircularProgress/><br/><br/><br/><br/></>: <RecipeList recipes={recipes} />}
       </Container>
     </>
   );
